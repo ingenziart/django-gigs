b73fe8a13b4bf93d21c8f0f82d1da7d42dceb65e
ingenziart <104985332+ingenziart@users.noreply.github.com>
2024-04-27 19:36:32 -0400

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,7 +1,26 @@
+"""
+URL configuration for real_estate project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
 from django.urls import path
-from . import views
+from listings.views import listing_list, listing_retrieve, listing_create
 
 urlpatterns = [
-    path("<int:month>", views.monthly_challenges_by_number),
-    path("<str:month>", views.mounthly_challenges),
+    path("admin/", admin.site.urls),
+    path("janu/", listing_list),
+    path("listings/<pk>/", listing_retrieve),
+    path("add-listing/", listing_create),
 ]
